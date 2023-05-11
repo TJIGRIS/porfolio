e766e1d3b1f8f5dd9da53541d5cf1fb3f87c7d84
TJIGRIS <johanalvarezz237@gmail.com>
2023-05-11 17:58:31 -0500

    todo el media queries casi terminado

--- src/componentes/inicio/NavbarComponente.jsx
+++ src/componentes/inicio/NavbarComponente.jsx
@@ -7,10 +7,14 @@ import { motion } from "framer-motion";
 // importar la funcion de la animacion
 import { animacionApararicion } from "../../assets/FramerMotion/animaciones";
 
+// impportar icons de react icons
+import { AiOutlineClose } from "react-icons/ai";
+import { HiBars3 } from "react-icons/hi2";
+
 export const NavbarComponente = () => {
   return (
     <nav className="active">
-      <div className="nav contenido">
+      <div className="nav contenido rela">
         {/* logo */}
         <div className="nav__logo rela">
           <a href="#">TJIGRIS</a>
@@ -50,6 +54,12 @@ export const NavbarComponente = () => {
             className="transicionW"
           ></motion.span>
         </ul>
+
+        {/* barras */}
+        <div className="toggle">
+          <HiBars3 className="icon" />
+          {/* {document.body.scrollWidth >= 100 ? <HiBars3 /> : <AiOutlineClose />} */}
+        </div>
       </div>
     </nav>
   );
